7ce094a0910cbda94c15e43a2566fc3f50ed8093
Kokan <56218348+kokanj@users.noreply.github.com>
2022-04-27 23:21:13 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 let chicken=document.getElementById('chicken');
 let salad=document.getElementById('salad');
-let papper=document.getElementById('papper');
+let papper=document.getElementById('paper');
 let onion=document.getElementById('onion');
 let wood=document.getElementById('wood');
+let text=document.getElementById('home-text');
 
 window.addEventListener('scroll', function(){
-    let value=window.scrollY;
-    wood.style.left=value + 'px';
+    let value = window.scrollY;
+    chicken.style.left = (value * 1.2) + 'px';
+    text.style.top = (value * .8) + 'px';
+    paper.style.left = (value * 1.2) + 'px';
+    salad.style.left = (value * -.8) + 'px';
+    onion.style.left = (value * -.5) + 'px';
+
     
 })
