82d746eb5c93f12b98c4e65e6f6771d4cad400cb
Kokan <56218348+kokanj@users.noreply.github.com>
2022-05-05 20:05:11 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,3 +15,28 @@ window.addEventListener('scroll', function(){
     salad.style.left = (value * -.8) + 'px';
     onion.style.left = (value * -.5) + 'px';    
 })
+
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
